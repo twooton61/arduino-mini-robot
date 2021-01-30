e31bc527c2e2d8f7cbcea06fbcbb1b94e8848db6
Terry Wooton <terry@oodle.io>
2021-01-29 22:01:16 -0500

    spider positions

--- src/main.cpp
+++ src/main.cpp
@@ -19,38 +19,50 @@ Robo::Servo robo_servo_front_right(robo_brain, DIGITAL_IO_PIN(11));
 Robo::Servo robo_servo_back_left(robo_brain, DIGITAL_IO_PIN(10));
 Robo::Servo robo_servo_back_right(robo_brain, DIGITAL_IO_PIN(9));
 
-Robo::OLED robo_oled(robo_brain);
-
 void setup()
 {
   Serial.begin(9600);
 
   Serial.println("setup");
 
+  pinMode(LED_BUILTIN , OUTPUT);
+
   robo_brain.setup();
+}
+namespace spider
+{
+  inline void base();
+  inline void sploot();
 }
 
 void loop()
 {
-  Serial.println("loop3");
+  spider::base();
 
-  robo_oled.clear();
+  delay(1000);
 
+  spider::sploot();
+
+  delay(1000);
+}
+
+namespace spider
+{
+inline void base()
+{
   robo_servo_front_right.set_angle(180);
   robo_servo_front_left.set_angle(0);
 
   robo_servo_back_left.set_angle(180);
   robo_servo_back_right.set_angle(0);
+}
 
-  delay(1000);
+inline void sploot()
+{
+  robo_servo_front_right.set_angle(90);
+  robo_servo_front_left.set_angle(90);
 
-  robo_oled.clear();
-
-  robo_servo_front_right.set_angle(0);
-  robo_servo_front_left.set_angle(180);
-
-  robo_servo_back_left.set_angle(0);
-  robo_servo_back_right.set_angle(180);
-
-  delay(1000);
+  robo_servo_back_left.set_angle(90);
+  robo_servo_back_right.set_angle(90);
+}
 }
